7285fed52e6d40fa7fdec4fc8279b42563796edd
Dann <Lesterdannlopez7@gmail.com>
2025-10-03 19:40:03 +0800

    Added Sample Database inside main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,16 @@ from fastapi import FastAPI
 
 api = FastAPI()
 
-@api.get("/")
+# Sample Database
+all_todos = [
+    {"todo_id": 1, "todo_name": "Sports", "todo_description": "Go to the gym"},
+    {"todo_id": 2, "todo_name": "Read", "todo_description": "Read 10 pages"},
+    {"todo_id": 3, "todo_name": "Shop", "todo_description": "Go shopping"},
+    {"todo_id": 4, "todo_name": "Study", "todo_description": "Study for exam"},
+    {"todo_id": 5, "todo_name": "Meditate", "todo_description": "Meditate 20 minutes"},
+]
 
+
+@api.get("/")
 def index():
     return {"message": "Hello World"}
